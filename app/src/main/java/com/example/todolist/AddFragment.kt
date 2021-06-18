dc0b5cac272fb5b8922d5a153c8c0b644a051272
BoburUzbek <hayitboyev.bobur@mail.ru>
2021-06-18 15:38:06 +0500

    first commit

--- app/src/main/java/com/example/todolist/AddFragment.kt
+++ app/src/main/java/com/example/todolist/AddFragment.kt
@@ -1,12 +1,16 @@
 package com.example.todolist
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.Toast
 import com.example.todolist.databinding.FragmentAddBinding
+import java.util.*
 
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
@@ -15,8 +19,9 @@ class AddFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    private var _binding : FragmentAddBinding? = null
-            private val binding get() = _binding!!
+    private var cal = Calendar.getInstance()
+    private var _binding: FragmentAddBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,14 +34,74 @@ class AddFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View{
+    ): View {
         _binding = FragmentAddBinding.inflate(inflater, container, false)
 
         binding.dateEdt.setOnClickListener {
-            Toast.makeText(requireActivity(), "edt click", Toast.LENGTH_SHORT).show()
-            binding.dateEdt.setText("17 June 2021")
+            getDate(binding.dateEdt)
+        }
+
+        binding.timeEdt.setOnClickListener {
+            getTime(binding.timeEdt)
         }
         return binding.root
+    }
+
+    private fun getTime(editText: EditText) {
+        val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+            cal.set(Calendar.HOUR_OF_DAY, hour)
+            cal.set(Calendar.MINUTE, minute)
+
+            var trueHour = ""
+            var trueMinute = ""
+
+            if (hour < 10) {
+                trueHour = formatCorrection(hour)
+            }
+            if (minute < 10) {
+                trueMinute = formatCorrection(minute)
+            }
+            editText.setText("$trueHour:$trueMinute")
+        }
+
+        TimePickerDialog(
+            requireActivity(),
+            timeSetListener,
+            cal.get(Calendar.HOUR_OF_DAY),
+            cal.get(Calendar.MINUTE),
+            true
+        ).show()
+    }
+
+    private fun getDate(editText: EditText) {
+
+        val year = cal.get(Calendar.YEAR)
+        val month = cal.get(Calendar.MONTH)
+        val day = cal.get(Calendar.DAY_OF_MONTH)
+
+
+        val dateSetListener = DatePickerDialog(
+            requireActivity(), { view, year, month, dayOfMonth ->
+                var trueMonth = ""
+                var trueDay = ""
+
+                if (month + 1 < 10) {
+                    trueMonth = formatCorrection(month + 1)
+                }
+                if (dayOfMonth < 10) {
+                    trueDay = formatCorrection(dayOfMonth)
+                }
+
+                editText.setText("$year:$trueMonth:$trueDay")
+            }, year, month, day
+        )
+
+        dateSetListener.show()
+    }
+
+    //date va timega 1-10 oralig'idagi sonlarga 0 qo'shish
+    private fun formatCorrection(int: Int): String {
+        return "0$int"
     }
 
     companion object {
